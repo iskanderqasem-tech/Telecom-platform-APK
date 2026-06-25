20d427ff1ee0aae5d068188b5ee39ed9d96e272a
iskanderqasem-tech <iskanderqasem@gmail.com>
2026-06-25 12:47:28 +1200

    Add React import to Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button, Card, CardContent, Container, TextField, Typography, Alert } from '@mui/material';
